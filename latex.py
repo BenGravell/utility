6477d18c73cb9039f5e572a0350094e0dadefae6
Ben <42848659+BenGravell@users.noreply.github.com>
2020-11-13 18:56:59 -0600

    Minor update

--- latex.py
+++ latex.py
@@ -1,11 +1,15 @@
-def print_latex_matrix(A):
+def print_latex_matrix(A, places=None):
     n = A.shape[0]
     m = A.shape[1]
     print('\\begin{bmatrix}')
-    for i,row in enumerate(A):
+    for i, row in enumerate(A):
         line = '    '
-        for j,col in enumerate(row):
-            line += str(col)
+        for j, entry in enumerate(row):
+            if places is None:
+                entry_str = str(entry)
+            else:
+                entry_str = '%.*f' % (places, entry)
+            line += entry_str
             if j < m-1:
                 line += ' & '
         if i < n-1:
@@ -17,17 +21,4 @@ def print_latex_matrix(A):
 
 if __name__ == "__main__":
     import numpy as np
-    A = np.array([[0.8,0.1],[0.1,0.8]])
-    B = np.array([[1.0,0.0],[0.0,1.0]])
-    a = np.array([[0.9]])
-    Aa = np.array([[0.0,1.0],[1.0,0.0]])[:,:,np.newaxis]
-    b = np.array([[0.0]])
-    Bb = np.array([[0.0,0.0],[0.0,0.0]])[:,:,np.newaxis]
-    Q = np.eye(2)
-    R = np.eye(2)
-    S0 = np.eye(2)
-
-    print_latex_matrix(A)
-    print_latex_matrix(B)
-    print_latex_matrix(Q)
-    print_latex_matrix(R)
+    print_latex_matrix(np.random.rand(3, 3).round(3), places=3)
